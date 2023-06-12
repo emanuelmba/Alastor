356588be42e581a07bb6c8285c69b0503ae96301
emanuelmba <emanuelbailo@gmail.com>
2023-06-12 14:37:46 -0300

    update mpago

--- src/data/Content.js
+++ src/data/Content.js
@@ -9,7 +9,7 @@ export const ContentProvider = ({ children }) => {
   const [authors] = useState(AuthorData)
   const [books] = useState(BookData)
   const mail = 'info@editorial-alastor.com.ar'
-  const mpago = 'https://mpago.la/1DNwc3V'
+  const mpago = 'https://mpago.la/23eHjLu'
   const prices = [1000, 1500, 3000]
 
   const [author, setAuthor] = useState({
